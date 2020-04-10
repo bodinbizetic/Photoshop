d6013f7a2141bf7676cc137f0589783469c661bf
Bodin Bizetic <dinbo99@gmail.com>
2020-04-11 00:22:21 +0200

    + simple operation class

---
+++ headers/simple_operation.h
@@ -0,0 +1,17 @@
+#ifndef __SIMPLE_OPERATION_H__
+#define __SIMPLE_OPERATION_H__
+
+#include <functional>
+
+#include "operation.h"
+
+class SimpleOperation : public Operation {
+public:
+    SimpleOperation(std::function<int(int)> operation_function_, std::string name_ = "");
+
+    virtual int operator() (int x) override;
+private:
+    std::function<int(int)> operation_function;
+};
+
+#endif // __SIMPLE_OPERATION_H__
---
+++ source/simple_operation.cpp
@@ -0,0 +1,11 @@
+#include "simple_operation.h"
+
+SimpleOperation::SimpleOperation(std::function<int(int)> operation_function_, std::string name_ )
+:   operation_function(operation_function_),
+    Operation(name_) {
+
+}
+
+int SimpleOperation::operator() (int x) {
+    return operation_function(x);
+}
