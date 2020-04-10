f8548d5e99cb5994eaf0c46a999779b287dd5997
Bodin Bizetic <dinbo99@gmail.com>
2020-04-11 00:52:08 +0200

    + virtual destructor and virtual copy

--- headers/operation.h
+++ headers/operation.h
@@ -12,16 +12,15 @@ class OperationNameAlreadyExists : public std::exception {
 class Operation {
 public:
     Operation(std::string name_ = "");
-    ~Operation();
+    virtual ~Operation();
     std::string Name() const { return name; }
     void setName(std::string name_);
 
     virtual int operator() (int val) = 0;
+    virtual Operation* copy() = 0;
 private:
-    static std::map<std::string, Operation*> all_operations;
     std::string name;
 
-    static void remove(std::string name_);
 };
 
 #endif // __OPERATION_H__
