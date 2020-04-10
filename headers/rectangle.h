b1404949db91b3a2e4297bcc70b05608415546e6
Bodin Bizetic <dinbo99@gmail.com>
2020-04-10 22:35:01 +0200

    + implementation of rectangle methods

--- headers/rectangle.h
+++ headers/rectangle.h
@@ -5,13 +5,15 @@
 
 class Rectangle {
 public:
-    Rectangle(int x, int y, int w, int h);
-    Rectangle(std::pair<int, int>, std::pair<int, int>);
+    Rectangle(int x, int y, int w, int h) : coordinates{x, y}, dimensions{w, h} {}
+    Rectangle(std::pair<int, int> coord, std::pair<int, int> dim) : coordinates(coord), dimensions(dim) {}
 
     bool inRectangle(std::pair<int, int>);
 private:
     std::pair<int, int> coordinates;
     std::pair<int, int> dimensions;
+
+    bool between(int coord, int dim, int dot);
 };
 
 #endif // __RECTANGLE_H__
