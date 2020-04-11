d04ec6e1a45c4fd35364f93d6f7adfd537e28706
Bodin Bizetic <dinbo99@gmail.com>
2020-04-11 20:28:38 +0200

    + method to combine Pixels

--- headers/pixel.h
+++ headers/pixel.h
@@ -6,7 +6,7 @@
 
 class Pixel {
 public:
-    Pixel(int red_=0, int green_=0, int blue_=0, int alfa_=100)
+    Pixel(int red_=0, int green_=0, int blue_=0, int alfa_=255)
     : red(red_), green(green_), blue(blue_), alfa(alfa_) {};
 
     int Red() const {return red;}
@@ -24,6 +24,9 @@ public:
     void setAlfa(std::function<int(int x)> fun) { alfa = setColor(fun(alfa)); }
     void setRGB(std::function<int(int x)> fun);
     void setRGBA(std::function<int(int x)> fun);
+
+    friend Pixel operator+(Pixel p1, Pixel p2);
+    friend int combineAlfa(int aA, int aB) { return aA + (aB * (255 - aA) / 255); }
 private:
     int red;
     int blue;
--- source/pixel.cpp
+++ source/pixel.cpp
@@ -1,5 +1,6 @@
-#include "pixel.h"
 #include <iostream>
+#include <functional>
+#include "pixel.h"
 
 void Pixel::setRGBA(std::function<int(int x)> fun){
     setRGB(fun);
@@ -11,6 +12,19 @@ void Pixel::setRGB(std::function<int(int x)> fun){
     setRed(fun);
     setGreen(fun);
     setBlue(fun);
+}
+
+Pixel operator+(Pixel p1, Pixel p2) {
+    std::function<int(int, int, int, int)> calc = [](int rA, int aA, int rB, int aB) -> int {
+        return (rA * aA / 255) + (rB * aB * (255 - aA) / (255*255));
+    };
+
+    int red = calc(p1.Red(), p1.Alfa(), p2.Red(), p2.Alfa());
+    int green = calc(p1.Green(), p1.Alfa(), p2.Green(), p2.Alfa());
+    int blue = calc(p1.Blue(), p1.Alfa(), p2.Blue(), p2.Alfa());
+    int alfa = p1.Alfa() + (p2.Alfa() * (255 - p1.Alfa()) / 255);
+
+    return Pixel(red, green, blue, alfa);
 }
 
 
