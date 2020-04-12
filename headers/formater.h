7ae4cf252f3902556568b8d416010575b1312671
Bodin Bizetic <dinbo99@gmail.com>
2020-04-12 22:40:02 +0200

    + interface for formater class

---
+++ headers/formater.h
@@ -0,0 +1,16 @@
+#ifndef __FORMATER_H__
+#define __FORMATER_H__
+
+#include <vector>
+#include <string>
+
+class Formater {
+public:
+    Formater(std::string path_);
+    virtual std::vector< std::vector<int> > load() = 0;
+    virtual void store(std::vector< std::vector<int> >) = 0;
+protected:
+    std::string path;
+};
+
+#endif // __FORMATER_H__
