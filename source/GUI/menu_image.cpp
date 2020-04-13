acb3a6ff578661fdb381f3f3ee2530a4dfac4251
Bodin Bizetic <dinbo99@gmail.com>
2020-04-13 23:04:07 +0200

    Open layer menu

--- source/GUI/menu_image.cpp
+++ source/GUI/menu_image.cpp
@@ -1,12 +1,14 @@
 #include <vector>
 
 #include "menu_image.h"
+#include "menu_layer.h"
 
 void Menu_Image::functionCall(std::string x) {
     if(x == "0")
         running = false;
     else if(x == "1"){
-        // TODO: layer menu
+        Menu_Layer m(project);
+        m.start();
     } else if(x == "2") {
         // TODO: operation menu
     } else if(x == "3") {
