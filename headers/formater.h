9d1722bf39bdf50be346951b18c1d473a41f5a7d
Bodin Bizetic <dinbo99@gmail.com>
2020-04-13 02:36:16 +0200

    + exception for filedoesnotexists

--- headers/formater.h
+++ headers/formater.h
@@ -4,9 +4,13 @@
 #include <vector>
 #include <string>
 
+class FileDoesNotExist {
+
+};
+
 class Formater {
 public:
-    Formater(std::string path_);
+    Formater(std::string path_) : path(path_) {}
     virtual std::vector< std::vector<int> > load() = 0;
     virtual void store(std::vector< std::vector<int> >) = 0;
 protected:
