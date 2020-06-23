5c7bde85b708294df222a02913c0731aa12cf4d3
Bodin Bizetic <dinbo99@gmail.com>
2020-06-23 23:22:19 +0200

    Add Constructor and Getter for Layer

--- src/photoshop/layer/Layer.java
+++ src/photoshop/layer/Layer.java
@@ -5,4 +5,43 @@ public class Layer {
     private String path;
     private int opacity;
     private boolean active;
+
+    public Layer(String name, String path, int opacity, boolean active) {
+        this.name = name;
+        this.path = path;
+        this.opacity = opacity;
+        this.active = active;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public int getOpacity() {
+        return opacity;
+    }
+
+    public void setOpacity(int opacity) {
+        this.opacity = opacity;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
 }
