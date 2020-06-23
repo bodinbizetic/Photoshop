0ab0be1666e17cc6d27cf0eea0b30216260202b2
Bodin Bizetic <dinbo99@gmail.com>
2020-06-23 23:23:00 +0200

    Move Project class to different packet

---
+++ src/photoshop/project/Project.java
@@ -0,0 +1,47 @@
+package photoshop.project;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import photoshop.exceptions.FileCorruptedException;
+import photoshop.layer.Layer;
+import photoshop.operations.Operation;
+import photoshop.selection.Selection;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Project {
+
+    private String path;
+    private String name;
+    private List<Selection> all_selections = new ArrayList<>();
+    private List<Operation> all_operations = new ArrayList<>();
+    private List<Layer> all_layers = new ArrayList<>();
+
+    public Project(String name, String path) {
+        this.path = path;
+        this.name = name;
+    }
+
+    Project(List<Selection> all_selections, List<Operation> all_operations, List<Layer> all_layers, String name, String path) {
+        this.all_selections = all_selections;
+        this.all_operations = all_operations;
+        this.all_layers = all_layers;
+    }
+
+    public static void main(String[] args) throws FileCorruptedException {
+        String path = "C:\\Users\\Dinbo\\Desktop\\POOP C++\\Photoshop\\myProj\\Project.xml";
+        ProjectLoader projectLoader = new ProjectLoader(path);
+        Project p = projectLoader.loadProject();
+
+        for(Layer l : p.all_layers)
+            System.out.println(l.getName() + " " + l.getPath());
+    }
+}
